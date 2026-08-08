c6710c9be79cc717c984beb9143e906f4cb072a7
shibamishra52-source <shibamishra52@gmail.com>
2026-08-08 07:58:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN
 
 bot = Bot(
-    "sargam_bot"
-    API_ID,
-    API_HASH,
+    "sargam_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="modules")
 )
