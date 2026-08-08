a67e7640a225165e5b14da485016279a54197e7e
shibamishra52-source <shibamishra52@gmail.com>
2026-08-08 01:04:14 -0700

    Update pmpermit.py

--- modules/pmpermit.py
+++ modules/pmpermit.py
@@ -1,9 +1,8 @@
-from callsmusic.callsmusic import client as USER
 from pyrogram import filters
-from pyrogram.types import Chat, Message, User
-
-
+from pyrogram.types import Message
+from callsmusic.callsmusic import client as USER
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n")
-  return                        
+async def pmPermit(client, message: Message)
+ await message.reply_text("⚠️ *Warning!* Owner is currently busy right now. "
+        "Please wait, we will talk to you when free. Do not spam!")
+                          
